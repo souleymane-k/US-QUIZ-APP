d1351b9b597c87ca12bf7f4a004f4bc4cb5df2f2
souleymane kone <desolo74@gmail.com>
2020-10-20 10:13:05 -0700

    new changed

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
+
 const store = {
   // 5 or more questions are required
   questions: [
-    {
+    { // Question 1
       question: 'Who was the president of the United States in 1960?',
       answers: [
         'Ronald Reagan',
-        'Dwight Eisenhower',
-        'Barack Obama',
-        'Jimmy Carter'
+          'Dwight Eisenhower',
+          'Barack Obama',
+          'Jimmy Carter'
       ],
       correctAnswer: 'Dwight Eisenhower'
     },
-    {
+    { // Question 2
       question: 'Who was the president of the United States in 1970?',
       answers: [
-        'Ronald Reagan',
-        'Jimmy Carter',
-        'Richard Nixon',
-        'Harry Truman'
+          'Ronald Reagan',
+          'Jimmy Carter',
+          'Richard Nixon',
+          'Harry Truman'
       ],
       correctAnswer: 'Richard Nixon'
     },
-    {
+    { // Question 3
       question: 'Who was the president of the United States in 1980?',
       answers: [
         'Bill Clinton',
@@ -31,37 +32,38 @@ const store = {
       ],
       correctAnswer: 'Jimmy Carter'
     },
-    {
+    { // Question 4
       question: 'Who was the president of the United States in 1990?',
-      answers: [
-        'George Bush',
-        'George W Bush',
-        'Bill Clinton',
-        'Richard Nixon'
-      ],
-      correctAnswer: 'George Bush'
+        answers: [
+          'George Bush',
+          'George W Bush',
+          'Bill Clinton',
+          'Richard Nixon'
+        ],
+        correctAnswer: 'George Bush'
     },
-    {
-      question: 'Who was the president of the United States in 2000?',
-      answers: [
-        'Barack Obama',
-        'George W Bush',
-        'Lyndon Johnson',
-        'Bill Clinton'
-      ],
-      correctAnswer: 'Bill Clinton'
+    { // Question 5
+      question: 'Who was the president of the United States in 2010?',
+        answers: [
+          'George W Bush',
+          'Donald J Trump',
+          'Barack Obama',
+          'Jimmy Carter'
+        ],
+        correctAnswer: 'Barack Obama'
+      
     },
-    {
-    question: 'Who was the president of the United States in 2010?',
-      answers: [
-        'George W Bush',
-        'Donald J Trump',
-        'Barack Obama',
-        'Jimmy Carter'
-      ],
-      correctAnswer: 'Barack Obama'
+    { // Question 6
+      question: 'Who was the president of the United States in 2010?',
+        answers: [
+          'George W Bush',
+          'Donald J Trump',
+          'Barack Obama',
+          'Jimmy Carter'
+        ],
+        correctAnswer: 'Barack Obama'
     },
-    {
+    { // Question 7
       question: 'Who is the current president of the United States 2020?',
       answers: [
         'George W Bush',
@@ -70,13 +72,48 @@ const store = {
         'Jimmy Carter'
       ],
       correctAnswer: 'Donald J Trump'
+    
     },
+    { // Question 8
+      question: 'Who was the president of the United States in 1990?',
+        answers: [
+          'George Bush',
+          'George W Bush',
+          'Bill Clinton',
+          'Richard Nixon'
+        ],
+        correctAnswer: 'George Bush'
+    },
+    { // Question 9
+      question: 'Who was the president of the United States in 1990?',
+        answers: [
+          'George Bush',
+          'George W Bush',
+          'Bill Clinton',
+          'Richard Nixon'
+        ],
+        correctAnswer: 'George Bush'
+    },
+    { // Question 10
+      question: 'Who was the president of the United States in 1990?',
+        answers: [
+          'George Bush',
+          'George W Bush',
+          'Bill Clinton',
+          'Richard Nixon'
+        ],
+        correctAnswer: 'George Bush'
+    }
   ],
   quizStarted: false,
   questionNumber: 0,
-  score: 0
-};
+  submittingAnswer: false,
+  score: 0,
 
+  currentQuestionState: {
+    answerArray: []
+  }
+};
 
 
 /**
@@ -84,7 +121,7 @@ const store = {
  * Technical requirements:
  * 
  * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material and access support for more details.
+ * See your course material, consult your instructor, and reference the slides for more details.
  *
  * NO additional HTML elements should be added to the index.html file.
  *
@@ -94,252 +131,258 @@ const store = {
  * 
  */
 
-/********** TEMPLATE GENERATION FUNCTIONS **********/
+               
+                        /********** TEMPLATE GENERATION FUNCTIONS **********/
 
-// These functions return HTML templates
-/* HTML template for the start pages */
-function generateStartScreenHtmlT(){
+                             // These functions return HTML templates
 
-  console.log('`generateStartScreenHtmlT` ran');
-  
+//++++++++++++++++++The HTML for the firstPage page, provides the button to begin quiz+++++++++++++++++
+
+function generateFirstPageHtml() {
   return `
-  <div id="startContainer">
-             <h2>This App Quiz will check your knowledge about U S History regards to presidency and the year served in office.</h2>
-             <a href="file:///Users/desolo/Desktop/US-QUIZ-APP/index.html" id="start">Start Quiz</a>
-    </div>`;
-
-}
-
-/* ----------------------------Template for the next page--------------------------------*/
-
-/* 
- HTML template that shows  the evolution  of question number and the Score 
-
- */
-
- function evolutionQuestionAndScore(){
-
-  console.log('`evolutionQuestionAndScore` ran' );
-  
-   return `
-    <ul class="result">
-    <li id="questionNumber">
-    Question Number: ${store.currentQuestion +1}/${store.questions.length}
-    </li>
-    <li id="score">
-    Score: ${store.score}/${store.questions.length}
-    </li>
-    </ul?
-   `;
-
- }
-
- /**
-  * ---------------------Answers Options for one question ----------------------------*/
-
-  function generateAnswerOptionsHtml(){
-
-    console.log('`generateAnswerOptionsHtml` ran' );
-    const answerOptionsArray = store.questions[store.currentQuestion].answers;
-    let answersOptionHtml = " ";
-     for (let i= 0; i<store.length; i++){
-    answersOptionsArray.forEach(answer => {
-      answersOptionHtml += `<div id="input-container${i}"> 
-      <input type="radio" id="option${i+1}" name="option" value="${answer}" index="$(i+1)" require/>
-      <label for="option${i+1}">${answer}</label>
+   <div id=container>
+  <div class="firstPage">
+    <form>
+      <p>
+        This Quiz will check your konwledge about US presidency history.
+      </p>
+      <button type="submit"id="StartQuizApp" autofocus>Start Quiz</button>
+    </form>
   </div>
-   `;
-      
-    });
-   return answersOptionHtml;
-  }
+  </div>
+    `;
 }
-   /* const answersOptionArray = store.questions[STORE.currentQuestion].answers;*/
-       /*};
+function generateStartQuestionHtml(questionObject) {
+  // console.log(questionObject);
+  // console.log(questionObject.question.answers);
+  return `
+    <div class='quiz-interface'>
+      <p> Question: ${questionObject.index}/${store.questions.length}</p>
+      <p>
+       ${questionObject.question.question}
+      </p>
 
-  
+      <form>
+      <ol type="1">
+        ${generateQuizAnswers(questionObject.question.answers)}
+      </ol>
+      <button type="submit" class="submit-answer">Submit Answer</button>
+      </form> 
+      <p>Score: ${store.score}</p>
+     </div>
+    `;
+}
 
-  /*--------------------- Generate Html template for one question -------------------------------*/
-  function generateQuestionHtmlTemplate(){
 
-    console.log('`generateQuestionHtmlTemplate` ran');
-    let currentQuestion = store.questions[store.currentQuestion];
+function generateAnswerResults(){
+  let answerArray = store.currentQuestionState.answerArray;
 
-    return `
-    <form id="question-form"  class="question-form" >
-    <div class="usQuestion">
-    <p> ${currentQuestion.question}</p>
-    </div>
-    <div class="option">
-      ${generateAnswerOptionsHTML()};
-    </div>
-    </div>
-     
-    `
-    /** double check the button */
-
+  const buttons = {
+    next: ' <button type="submit" class="next-question" autofocus>Next Question</button>',
+    results: '<button type="submit" class="see-results" autofocus>See Results</button>'
   };
 
-  /** Generate html template for the result */
-  function generateResultsScreen() {
-    console.log('`generateResults, ran');
-    return `
-      <div class="results">
-        <form id="js-restart-quiz">
-            <div class="group">
-              <div class="item-double">
-                <label>Your Score is: ${store.score}/${store.questions.length}</label>
-              </div>
-              <div class="item">
-                <button type="button" id="restart"> Restart Quiz </button>
-              </div>
-              </div>
-          </form>
-      </div>
-    `;
-  }
+  let correctResponse = `"${answerArray[1]}" is correct`;
+  let incorrectResponse = `${answerArray[2]} is not correct. The correct answer is<br><br>
+  "${answerArray[1]}"`;
+
+  let isLastQuestion = (store.questionNumber + 1) === (store.questions.length);
   
-/**User with feedback weather or not their result is correct or not */
-function generateFeedback(){
-  console.log('`generateFeedback, ran`');
-  let correctAnswer = store.questions[STORE.currentQuestion].correctAnswer;
-  let html = '';
-  if (answerStatus === 'correct') {
-    html = `
-    <div class="right-answer">That is correct!</div>
-    `;
-  }
-  else if (answerStatus === 'incorrect') {
-    html = `
-      <div class="wrong-answer">That is incorrect. The correct answer is ${correctAnswer}.</div>
-    `;
-  }
-  return html;
+  return `
+    <div class="answer-response" id="container">
+    <form>
+    <p>${answerArray[0] === true ? correctResponse : incorrectResponse}</p>
+    <p> Score: ${store.score}</p>
+   ${isLastQuestion ? buttons.results : buttons.next}
+    </form>
+    </div>
+  `;
 }
 
-/********** RENDER FUNCTION(S) **********/
-function render(){
-  event.preventDefault();
-  console.log('render, ran');
-  let html = " ";
-  if (store.currentQuizStarted === false){
-    $('main').html(generateStartScreenHtmlT());
+
+function generateQuizAnswers(answers){
+  let answerArray = [];
+  let indexArray = [];
+  answers.forEach(answer => {
+    answerArray.push(answer);
+    indexArray.push(answers.indexOf(answer));
+  });
+  //console.log(indexArray);
+  return answerArray.map(answer => stringifyAnswerArray(answer)).join('');
+}
+
+function stringifyAnswerArray(answer){
+  let questionNumber = store.questionNumber;
+  let name = store.questions[questionNumber].answers.indexOf(answer);
+  return `
+  <div id=container>
+    <li>
+      <div class="answer-container" >
+      <input type="radio" name="answer" id="answer-${name}" data-answer="${answer}" require>
+      <label for="answer-${name}"> ${answer}</label>
+     
+      </div>
+    </li>
+    </div>
+  `;
+}
+
+function generateQuizResultsString(){
+  return `
+    <div class='quiz-results' id="container">
+      <p>
+       The Quiz is over.
+         </p>
+          <p>You scored ${store.score} out of ${store.questions.length}</p>            
+        <button class="restart-quiz">Restart Quiz</button>      
+    </div>   
+    
+`;
+        }
+
+
+
+/**********************************RENDER FUNCTION(S) ********************************************/
+
+function renderQuiz () {
+
+  if(store.quizStarted === false) {
+    if(store.questionNumber === store.questions.length){
+      const quizResultsString = generateQuizResultsString();
+      $('main').html(quizResultsString); 
+    } else {
+      const quizWelcomeInterfaceString = generateFirstPageHtml();
+      $('main').html(quizWelcomeInterfaceString);
+    }
+  } else if (store.quizStarted === true) {
+    if(store.submittingAnswer === false) {
+      const quizInterfaceString = generateStartQuestionHtml(currentQuestion());
+      $('main').html(quizInterfaceString);
+    } else if (store.submittingAnswer === true) {
+      const quizAnswerResponseString = generateAnswerResults();
+      $('main').html(quizAnswerResponseString);
+    }
+  } 
+}
+
+
+// Changes the state of the application to a quizStarted = true
+function startQuiz() {
+  //console.log('quiz has begun');
+  store.quizStarted = true;
+}
+
+// currentQuestion
+function currentQuestion(){
+  let index = store.questionNumber;
+  let questionObject = store.questions[index];
+  return {
+    index: index +1,
+    question: questionObject
+  };
+}
+
+// Go to the next question of the quiz
+// Model function changes state
+function nextQuestion(){
+  if (store.questionNumber < store.questions.length){
+    store.questionNumber++;
+    store.submittingAnswer =false;
+   // console.log(store.questionNumber);
+  } else if(store.questionNumber === store.questions.length) {
+    store.quizStarted = false;
+  }
+}
+
+
+function validateCorrectAnswer() {
+  let radios = $('input:radio[name=answer]');
+  let selectedAnswer = $('input[name="answer"]:checked').data('answer');
+  let questionNumber = store.questionNumber;
+  let correctAnswer = store.questions[questionNumber].correctAnswer;
+
+  if (radios.filter(':checked').length === 0) {
+    //alert('Please select an answer.');
     return;
-  }
-  else if (store.currentQuestion >= 0 && store.currentQuestion <store.questions.length){
-    html = evolutionQuestionAndScore();
-    html += generateQuestionHtmlTemplate()
-    $('main').html(html);
-  }
-  else{
-    $('main').html(generateResultsScreen());
-  }
-}
-
-
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
-
-/********** EVENT HANDLER FUNCTIONS **********/
-
-// These functions handle events (submit, click, etc)
-/* handle click start button -----------------------------*/
-function handleStartClicked(){
-  console.log(handleStartClicked);
-  $('main').on('click', '#start', function (event) {
-    store.quizStarted = true;
-    render();
-  });
-}
-/* handle click next button -----------------------------*/
-function handleNextQuestionbutton(){
-$('body').on('click', '#btn-next', (evnt)=>{
-
-  render();
-});
-
-}
-/* handle submit button for the submission of the form */
-function handleFormSubmission(){
-  $('body').on('submit', '#question-form', function (event) {
-    event.preventDefault();
-    const currentQuestion = store.questions[store.currentQuestion]
-    /** let the user checked box*/
-    let selectedOption = $('input[name=options]:checked').val();
-
-
-    /**
-     * Creates an id '#input-container' + the index of 
-     * the current question in the answers array.
-     * 
-     * Example: #input-container-0
-     */
-    let optionContainerId = `#input-container-${currentQuestion.answers.findIndex(i => i === selectedOption)}`;
-
-    if (selectedOption === currentQuestion.correctAnswer) {
-      store.score++;
-      $(optionContainerId).append(generateFeedback('correct'));
+  } else {
+    store.submittingAnswer = true;
+    if(selectedAnswer === correctAnswer){
+      store.score += 1;
+      store.currentQuestionState.answerArray = [true, correctAnswer, selectedAnswer];
+    } else {
+      store.currentQuestionState.answerArray = [false, correctAnswer, selectedAnswer];
     }
-    else {
-      $(optionContainerId).append(generateFeedback('incorrect'));
-    }
-    store.currentQuestion++;
-    // hide the submit button
-    $('#btn-sbt').hide();
-    // disable all inputs
-    $('input[type=radio]').each(() => {
-      $('input[type=radio]').attr('disabled', true);
-    });
-    // show the next button
-    $('#btn-next').show();
-
-  });
+  }
 }
-/**
- * Reset the all data to  restart the quiz
- */
+
+function seeResults() {
+  store.quizStarted = false;
+  store.questionNumber ++;
+}
+
 function restartQuiz() {
   store.quizStarted = false;
-  store.currentQuestion = 0;
-  store.score = 0;
+  store.questionNumber = 0;
+  store.submittingAnswer = false;
+  store.currentQuestionState.answerArray = [];
 }
 
-function handleRestartButton() {
-  $('body').on('click', '#restart', () => {
+/********** EVENT HANDLER FUNCTIONS **********/
+// These functions handle events (submit, click, etc)
+// Controller layer
+
+function handleBeginQuizSubmit(){
+  
+  $('main').on('click', '#StartQuizApp', (event) =>{
+    event.preventDefault();
+    startQuiz();
+    renderQuiz();
+  });
+}
+
+function handleSubmitAnswer() {
+  $('main').on('click' , '.submit-answer', (event)=>{
+    event.preventDefault();
+    //console.log('submitting answer');
+    validateCorrectAnswer();
+    renderQuiz();
+  });
+}
+
+function handleNextQuestionSubmit(){
+  $('main').on('click', '.next-question', (event) => {
+    event.preventDefault();
+    nextQuestion();
+    renderQuiz();
+  });
+}
+
+function handleSeeResultsSubmit(){
+  $('main').on('click', '.see-results', (event) => {
+    event.preventDefault();
+    seeResults();
+    renderQuiz();
+  });
+}
+
+function handleRestartQuizSubmit(){
+  $('main').on('click', '.restart-quiz', (event) => {
+    event.preventDefault();
     restartQuiz();
-    render();
+    renderQuiz();
   });
 }
 
 
-function handleQuizApp(){
-  render();
-  handleStartClicked();
-  handleNextQuestionbutton();
-  handleFormSubmission();
-  handleRestartButton();
-}
-
-$(handleQuizApp);
-
-// Html template generator //
-
-
-/*function select(){
-  console.log(select);
-  $('#start').after(h3Template);
-  
-}
-
-function callBack(){
-  
-  $('#start').click(select);
+// This function will launch all other functions after the page is loaded
+function handleQuiz (){
+  renderQuiz();
+  handleBeginQuizSubmit();
+  handleSubmitAnswer();
+  handleNextQuestionSubmit();
+  handleRestartQuizSubmit();
+  handleSeeResultsSubmit();
 
 }
-function h3Template(){
-  return '<h3>Souleymane Kone</h3>';
-}
 
-$(callBack);*/
-
-
-
+$(handleQuiz);
